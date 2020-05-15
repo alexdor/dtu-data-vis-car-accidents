4336b223bdb30682438da01357b3f06bf23279a3
Alexandros Dorodoulis <aldor94@gmail.com>
2020-05-16 00:14:51 +0200

    feat: Update notebook

--- src/components/Intro.svelte
+++ src/components/Intro.svelte
@@ -10,26 +10,25 @@
     {
       title: "Crashes per day of the week",
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
+        "The accidents distributions over the week, its immediately clear that Sunday (1) has the least accidents which is to be assumed considering it is the common day off resulting in less cars on the road. Oppositely Friday (6) is the most accident prone day, likely due to people being tired from a long week.",
       svg: crashDayOfWeek
     },
     {
       title: "Crashes by the hour",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
+      description: "The accidents distributed over the hours.",
       svg: crashHour
-    },
-    {
-      title: "Distance from each accident to nearest school",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
-      svg: kdeAccidentToSchool
     },
     {
       title: "Hours of accidents",
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
+        "The KDE plots the shows how many accidents happen spaced over the duration of 24 hours. We see three notable spikes. Fistly 7-9 o'clock which is when a large majority of the working populations will set of to leave their kids at school and/or drive to work. The next spike of the day is 17-19 o'clock which in turn is when people return from work. Lastly we have the time at 12 o'clock at night, which would clearly indicate the partying population starting to move around.",
       svg: kdeHoursOfAccidents
+    },
+    {
+      title: "Distance from each accident to nearest school",
+      description:
+        "The distance from each accident to the nearest elementrary school was calculated and the plot below shows the kde distribution of the findings. It was found that a large majority happen close to the schools, supporting what we have found.",
+      svg: kdeAccidentToSchool
     }
   ];
 
